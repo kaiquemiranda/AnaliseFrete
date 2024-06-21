350a845100209194acff82a5bc1b848c33ac15b3
Kaique Miranda <kaique.miranda1910@gmail.com>
2024-06-21 12:11:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
 
-vendas = pd.read_csv('csv/20240618_Vendas_BR_Mercado_Livre_2024-06-18_12-26hs_53766196 1(Vendas BR).csv', sep = ';', encoding='latin1', decimal=',')
+st.sidebar.image('box7.png')
+st.title('Vendas com Frete Alto')
 
-vendas['frete real'] = vendas['Tarifas de envio'] + vendas['Receita por envio (BRL)']
-fretePositivo = vendas['frete real'] - vendas['frete real'] - vendas['frete real']
-vendas['Porcentagem frete'] = fretePositivo / (vendas['Receita por produtos (BRL)'] / 100)
-frete30 = vendas[['N.º de venda', 'SKU', 'Receita por produtos (BRL)', 'Porcentagem frete', 'frete real']]
-maior_frete = frete30[frete30['Porcentagem frete'] > 30].sort_values('Porcentagem frete')
+file = st.file_uploader("Carregue o arquivo em formato CSV", type=["csv"])
 
-maior_frete.sort_values('Porcentagem frete').plot.bar(x='SKU', y='Porcentagem frete')
-plt.title('Vendas com frete maior que 30%')
-plt.ylabel('Porcentagem frete')
-plt.xlabel('')
-plt.xticks(rotation=45, ha='right')
-plt.show()
+# Verificar se um arquivo foi carregado
+if file is not None:
+    vendas = pd.read_csv(file, sep=';', encoding='latin1', decimal=',')
+
+    # Calculo de porcentagem de frete
+    vendas['frete real'] = vendas['Tarifas de envio'] + vendas['Receita por envio (BRL)']
+    fretePositivo = vendas['frete real'] - vendas['frete real'] - vendas['frete real']
+    vendas['Porcentagem frete'] = fretePositivo / (vendas['Receita por produtos (BRL)'] / 100)
+    frete30 = vendas[['N.º de venda', 'SKU', 'Receita por produtos (BRL)', 'Porcentagem frete', 'frete real']]
+    maior_frete = frete30[frete30['Porcentagem frete'] > 30].sort_values('Porcentagem frete')
+
+
+    fig = px.bar(maior_frete, x='SKU', y='Porcentagem frete',
+                 title='Vendas com frete maior que 30%',
+                 labels={'Porcentagem frete': 'Porcentagem frete', 'N.º de venda': 'N.º de venda'},
+                 text='Porcentagem frete')
+
+    # Ajustar a rotação do eixo x e alinhamento dos rótulos
+    fig.update_xaxes(tickangle=45, tickmode='linear')
+
+    # Exibir gráfico usando Streamlit
+    if frete30 is not None:
+        st.markdown("----")
+        st.plotly_chart(fig)
+        st.markdown("----")
+        st.write(maior_frete)
+    else:
+        st.markdown("----")
+        st.markdown("Não há vendas com frete maior que 30% no período informado")
+else:
+    st.write("Por favor, carregue um arquivo CSV.")
