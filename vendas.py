108bee911cbd530b99f07c4324bf182cd8b10f1d
Kaique Miranda <kaique.miranda1910@gmail.com>
2024-07-08 10:51:41 -0300

    Update vendas.py

--- vendas.py
+++ vendas.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.sidebar.image('box7.png')
 st.title('Vendas com Frete Alto')
 
-file = st.file_uploader("Carregue o arquivo em formato CSV", type=["csv"])
+file = st.sidebar.file_uploader("Carregue o arquivo em formato CSV", type=["csv"])
 
 # Verificar se um arquivo foi carregado
 if file is not None:
@@ -17,6 +17,7 @@ if file is not None:
     vendas['Porcentagem frete'] = fretePositivo / (vendas['Receita por produtos (BRL)'] / 100)
     frete30 = vendas[['N.º de venda', 'SKU', 'Receita por produtos (BRL)', 'Porcentagem frete', 'frete real']]
     maior_frete = frete30[frete30['Porcentagem frete'] > 30].sort_values('Porcentagem frete')
+    media = vendas['frete real'].mean()
 
 
     fig = px.bar(maior_frete, x='SKU', y='Porcentagem frete',
@@ -29,10 +30,15 @@ if file is not None:
 
     # Exibir gráfico usando Streamlit
     if frete30 is not None:
-        st.markdown("----")
-        st.plotly_chart(fig)
-        st.markdown("----")
-        st.write(maior_frete)
+        with st.expander("Vendas com frete maior que 30%"):
+            st.markdown("----")
+            st.plotly_chart(fig)
+            st.markdown("----")
+            st.write(maior_frete)
+            st.markdown("----")
+        with st.expander("Média de frete"):
+            st.markdown(f'{media:.2f}')
+
     else:
         st.markdown("----")
         st.markdown("Não há vendas com frete maior que 30% no período informado")
