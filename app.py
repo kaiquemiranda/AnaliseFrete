92b096cbe714676087f0077585e5578f23b13ad2
Kaique Miranda <kaique.miranda1910@gmail.com>
2024-08-15 15:28:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import openpyxl
 
-st.sidebar.image('box7.png')
-st.title('Vendas com Frete Alto')
+st.image('box7.png')
+st.title('Vendas com frete acima de 30%')
 
-file = st.file_uploader("Carregue o arquivo em formato CSV", type=["csv"])
+file = st.file_uploader("Escolha um arquivo Excel", type="xlsx")
 
 # Verificar se um arquivo foi carregado
 if file is not None:
-    vendas = pd.read_csv(file, sep=';', encoding='latin1', decimal=',')
+    vendas = pd.read_excel(file, sheet_name='Vendas BR', engine='openpyxl', skiprows=5)
 
     # Calculo de porcentagem de frete
     vendas['frete real'] = vendas['Tarifas de envio'] + vendas['Receita por envio (BRL)']
@@ -17,6 +18,7 @@ if file is not None:
     vendas['Porcentagem frete'] = fretePositivo / (vendas['Receita por produtos (BRL)'] / 100)
     frete30 = vendas[['N.º de venda', 'SKU', 'Receita por produtos (BRL)', 'Porcentagem frete', 'frete real']]
     maior_frete = frete30[frete30['Porcentagem frete'] > 30].sort_values('Porcentagem frete')
+    media = vendas['frete real'].mean()
 
 
     fig = px.bar(maior_frete, x='SKU', y='Porcentagem frete',
@@ -29,12 +31,17 @@ if file is not None:
 
     # Exibir gráfico usando Streamlit
     if frete30 is not None:
-        st.markdown("----")
-        st.plotly_chart(fig)
-        st.markdown("----")
-        st.write(maior_frete)
+        with st.expander("Vendas com frete maior que 30%"):
+            st.markdown("----")
+            st.plotly_chart(fig)
+            st.markdown("----")
+            st.write(maior_frete)
+            st.markdown("----")
     else:
         st.markdown("----")
         st.markdown("Não há vendas com frete maior que 30% no período informado")
+
+    with st.expander("Média de frete"):
+        st.markdown(f'{media:.2f}')
 else:
     st.write("Por favor, carregue um arquivo CSV.")
